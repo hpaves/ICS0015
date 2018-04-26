705474e380e9aa43fd0a0ec20d4eb6132f3fd2b3
hpaves <henri.paves@gmail.com>
2018-04-26 16:38:24 +0300

    app works, but always asks for filename when saving and gives FileNotFoundError when canceling out of a dialogue

--- homework_3/notepad.py
+++ homework_3/notepad.py
@@ -1,53 +1,68 @@
 import tkinter
 from tkinter import *
-from tkinter import messagebox, scrolledtext
+from tkinter import messagebox, scrolledtext, filedialog
 
 # I'm relying on a lot of example code from the lectures by Tauseef Ahmed
 
 # Define main window
 root = Tk()
+root.geometry("720x400")
+root.title("Hnote")
 
 # Create a frame for the text area
 textFrame = Frame(root)
 textFrame.pack()
 
-# Callback function for the Exit menu item
-def _quit(): 
-    root.quit()
-    root.destroy()
-    exit()
-
-# Callback function for the About menu item
-def aboutWindow():
-    tkinter.messagebox.showinfo("About", "Hnote v0.1\nBy Henri Paves\nThank you, Tauseef Ahmed")
-
-# Use ScrolledText
-scrollW = 100
-scrollH = 40
-scr = tkinter.scrolledtext.ScrolledText(root, width = scrollW, height = scrollH, wrap = tkinter.WORD)
+# Use ScrolledText widget
+scr = scrolledtext.ScrolledText(root, wrap = tkinter.WORD)
 scr.pack(side = LEFT, fill = BOTH, expand = YES)
 
-# Fill with content for testing purposes
-txt = '''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut sapien quis tellus sodales tincidunt at nec enim. Integer tempor odio quis sem condimentum tempus. Donec et lectus neque. Suspendisse gravida, tortor sed dapibus imperdiet, neque mi auctor nulla, id laoreet libero ligula non elit. Duis venenatis in orci sit amet varius. Nullam in urna ex. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc pharetra finibus lobortis. Ut rhoncus risus dolor, et tincidunt sapien efficitur id. Donec eu quam justo. Etiam feugiat velit quis consectetur dictum.
+# I want to credit Madis Võrklaev for the idea of putting all the menu functions into a single class
+class MenuItems:
+    def __init__(self, scr):
+        self.scr = scr
 
-Nulla mauris ligula, porttitor quis lorem fringilla, vulputate placerat tortor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi eget diam id tortor ullamcorper sodales. Pellentesque eu semper nulla, vitae imperdiet tortor. Nunc gravida sodales sollicitudin. Suspendisse nisi velit, bibendum ut augue pulvinar, cursus laoreet dui. Phasellus sit amet tincidunt lorem.'''
-scr.insert(tkinter.INSERT,txt)
+    def _new(self):
+        self.scr.delete(1.0, END)
 
-# Create the menu bar
+    def _open(self):
+        root.filename = filedialog.askopenfilename(title = "Select file",filetypes = (("text files","*.txt"),("all files","*.*")))
+        with open(root.filename, "r+") as activefile:
+            contents = activefile.read()
+            scr.delete(1.0,END)
+            scr.insert(tkinter.INSERT, contents)
+
+    def _save(self):
+        root.filename = filedialog.asksaveasfilename(title = "Select file",filetypes = (("text files","*.txt"),("all files","*.*")))
+        with open(root.filename, "w") as activefile:
+            contents = scr.get(1.0,END)
+            activefile.write(contents)
+            activefile.close
+
+    def _quit(self): 
+        root.quit()
+        root.destroy()
+        exit()
+
+    def _about(self):
+        messagebox.showinfo("About", "Hnote v0.1\nBy Henri Paves\nThank you, Tauseef Ahmed")
+
+# Create the menu bar and the menuitems object
 menuBar = tkinter.Menu(root) 
 root.config(menu = menuBar)
+hnote = MenuItems(scr)
 
 # Create the menu items and assign them to submenus
 fileMenu = Menu(menuBar, tearoff = 0)
-fileMenu.add_command(label = 'New')
-fileMenu.add_command(label = 'Open')
-fileMenu.add_command(label = 'Save')
+fileMenu.add_command(label = 'New', command = hnote._new)
+fileMenu.add_command(label = 'Open', command = hnote._open)
+fileMenu.add_command(label = 'Save', command = hnote._save)
 fileMenu.add_separator()
-fileMenu.add_command(label = 'Exit', command = _quit) 
+fileMenu.add_command(label = 'Exit', command = hnote._quit) 
 menuBar.add_cascade(label = 'File', menu = fileMenu)
 
 helpMenu = Menu(menuBar, tearoff = 0)
-helpMenu.add_command(label ='About', command = aboutWindow)
+helpMenu.add_command(label ='About', command = hnote._about)
 menuBar.add_cascade(label = 'Help', menu = helpMenu)
 
 # Start the main window (root window)

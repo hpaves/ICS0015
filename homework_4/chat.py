887aeb35859c4b5b79721f1ecc137d2c786b394e
hpaves <henri.paves@gmail.com>
2018-05-08 15:02:53 +0300

    gui window opens while chat still remains working

--- homework_4/chat.py
+++ homework_4/chat.py
@@ -1,14 +1,34 @@
 import socket
 import threading
 import sys
+import tkinter
+from tkinter import *
+from tkinter import messagebox, scrolledtext
+
+# I'm relying on a lot of example code from the lectures by Tauseef Ahmed
+
+# Define main window
+root = Tk()
+root.geometry("720x400")
+root.title("chat.py gui")
+
+# Create a frame for the text area
+textFrame = Frame(root)
+textFrame.pack()
+
+# Use ScrolledText widget
+scr = scrolledtext.ScrolledText(root, wrap = tkinter.WORD)
+scr.pack(side = LEFT, fill = BOTH, expand = YES)
 
 # followed the howCode simple python chat server tutorial https://www.youtube.com/watch?v=D0SLpD7JvZI
+
+port = 10000
 
 class Server:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # using ipv4 and tcp
     connections = []
     def __init__(self): # constructor
-        self.sock.bind(("0.0.0.0", 10000)) # bind socket to an address:port
+        self.sock.bind(("0.0.0.0", port)) # bind socket to an address:port
         self.sock.listen(1)
         print("Server running...")
 
@@ -43,7 +63,7 @@ class Client:
             self.sock.send(bytes(input(""), "utf-8"))
 
     def __init__(self, address):
-        self.sock.connect((address, 10000))
+        self.sock.connect((address, port))
 
         # we put sendMsg to run in the background
         iThread = threading.Thread(target=self.sendMsg) # new thread for sending data
@@ -56,8 +76,18 @@ class Client:
                 break
             print(str(data, "utf-8"))
 
+# class GUI(threading):
+#     def __init__(self, tk_root):
+#         self.root = tk_root
+#         threading.Thread.__init__(self)
+#         self.start()
+
+
 if (len(sys.argv) > 1): # if more than one command line argument
-    client = Client(sys.argv[1]) # connect client to second variable
+    loop_active = True # https://gordonlesti.com/use-tkinter-without-mainloop/
+    while loop_active:
+        root.update()
+        client = Client(sys.argv[1]) # connect  to second variable
 else:
     server = Server()
     server.run()

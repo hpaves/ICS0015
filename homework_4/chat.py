9dec3969b77e563762d32621abd61393a7f8c256
hpaves <henri.paves@gmail.com>
2018-05-18 02:03:20 +0300

    text based chat works, but commenting in the lines 120-121 to put the received text into the gui gives the 'main thread is not in main loop' RuntimeError

--- homework_4/chat.py
+++ homework_4/chat.py
@@ -11,19 +11,9 @@ from tkinter import messagebox, scrolledtext
 root = Tk()
 root.title("chat.py gui")
 
-# Create a frame for the text area
-# textFrame = Frame(root)
-# textFrame.pack()
-# sframe = Frame(textFrame)
-# sframe.pack(anchor='w')
-
-# Use ScrolledText widget
-# scr = scrolledtext.ScrolledText(root, wrap = tkinter.WORD)
-# scr.pack()
-
 # followed the howCode simple python chat server tutorial https://www.youtube.com/watch?v=D0SLpD7JvZI
 
-port = 10001
+port = 10000
 
 class Server:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # using ipv4 and tcp
@@ -45,18 +35,22 @@ class Server:
                     print(str(data, "utf-8"))
             if not data: # when client is disconneted
                 print(str(a[0]) + ":" + str(a[1]), "disconnected") # break tuple into two
+                print(str(6 - len(self.connections)) + " slots left")
                 self.connections.remove(c)
                 c.close()
                 break
 
     def run(self):
         while True:
-            c, a = self.sock.accept() # accept the connection
-            cThread = threading.Thread(target=self.handler, args=(c, a)) # put connection on a thread to receive data
-            cThread.daemon = True # program can exit when a thread is running
-            cThread.start()
-            self.connections.append(c)
-            print(str(a[0]) + ":" + str(a[1]), "connected")
+            if len(self.connections) < 5:
+                c, a = self.sock.accept() # accept the connection
+                cThread = threading.Thread(target=self.handler, args=(c, a)) # put connection on a thread to receive data
+                cThread.daemon = True # program can exit when a thread is running
+                cThread.start()
+                self.connections.append(c)
+                print(str(a[0]) + ":" + str(a[1]), "connected")
+                print(str(5 - len(self.connections)) + " slots left")
+
 
 # class Client:
 #     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -79,25 +73,28 @@ class Server:
 #                 break
 #             print(str(data, "utf-8"))
 
+
 class App(threading.Thread): # https://github.com/praven0894/Chat-with-tkinter-and-socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def sendMsg(self):
+    def SendMsg(self):
         while True:
             self.sock.send(bytes(input(""), "utf-8"))
 
     def __init__(self, address):
         threading.Thread.__init__(self)
         self.sock.connect((address, port))
-        iThread = threading.Thread(target=self.Send) # new thread for sending data
+        iThread = threading.Thread(target=self.SendMsg) # new thread for sending data
         iThread.daemon = True
         iThread.start()
+        jThread = threading.Thread(target=self.Receive) # new thread for receiving data
+        jThread.daemon = True
+        jThread.start()
 
         frame = Frame()
         frame.pack()
         self.gettext = scrolledtext.ScrolledText(frame, height=30,width=80)
         self.gettext.pack()
-        self.gettext.configure(state='disabled')
         sframe = Frame(frame)
         sframe.pack(anchor='w')
         self.sendtext = Entry(sframe,width=80)
@@ -105,36 +102,24 @@ class App(threading.Thread): # https://github.com/praven0894/Chat-with-tkinter-a
         self.sendtext.bind(sequence="<Return>", func=self.Send)
         self.sendtext.pack(side=LEFT)
 
-    def Send(self, args):
-        self.gettext.configure(state='normal')
+    def Send(self, event = None): # Thanks for the "event = None" tip, Madis Võrklaev
         text = self.sendtext.get()
         self.sock.send(bytes(text, "utf-8")) # send to server (broken)
         self.gettext.insert(END,'%s\n'%text) # print to chat
-        print(text) # print to console for testing
-        print(args)
+        # print(text) # print to console for testing
+        # print(args)
         self.sendtext.delete(0,END)
         self.sendtext.focus_set()
-        self.gettext.configure(state='disabled')
         self.gettext.see(END)
 
-    def run(self):
-        Receive(self.sock, self.gettext)
-
-class Receive():
-  def __init__(self, sock, gettext):
-    self.sock = sock
-    self.gettext = gettext
-    while 1:
-      try:
-        text = self.sock.recv(1024)
-        if not text: 
-            break
-        self.gettext.configure(state='normal')
-        self.gettext.insert(END,'%s\n'%text)
-        self.gettext.configure(state='disabled')
-        self.gettext.see(END)
-      except:
-        break
+    def Receive(self):
+        while True:
+            text = self.sock.recv(1024)
+            if not text:
+                break
+            # self.gettext.insert(tkinter.END,'%s\n'%text)
+            # self.gettext.see(END)
+            print(str(text, "utf-8"))
 
 
 if (len(sys.argv) > 1): # if more than one command line argument
